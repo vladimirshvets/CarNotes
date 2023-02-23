708dab56b562eafe99f90c2ed9baf701b4737fc2
Vladimir Shvets <uladzimir.shvets@gmail.com>
2023-02-23 17:39:10 +0300

    Web API project

---
+++ CarNotesAPI/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace CarNotesAPI.Models
+{
+    public class Car
+	{
+		public int Id { get; set; }
+
+		public string Make { get; set; }
+
+		public string Model { get; set; }
+
+		public string Generation { get; set; }
+
+		public string VIN { get; set; }
+
+		public int Year { get; set; }
+	}
+}
---
+++ CarNotesAPI/Models/Garage.cs
@@ -0,0 +1,11 @@
+﻿namespace CarNotesAPI.Models
+{
+    public class Garage
+	{
+		public int Id { get; set; }
+
+		public int UserId { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+	}
+}
